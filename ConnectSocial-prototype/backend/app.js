d7f494f21552ef88097b1fd46807de6cc60a97b8
Yiu Cheung <58731088+yiucheung0512@users.noreply.github.com>
2025-03-02 10:35:41 +0800

    Update app.js

--- ConnectSocial-prototype/backend/app.js
+++ ConnectSocial-prototype/backend/app.js
@@ -55,7 +55,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
